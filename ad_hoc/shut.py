84513eacd143e8dd9db5c849787bd7da7da7dc65
Bryan <brybyrne@cisco.com>
2021-03-12 08:51:10 -0500

    Bounce Interface Scripts

--- ad_hoc/shut.py
+++ ad_hoc/shut.py
@@ -1,5 +1,6 @@
 from netmiko import ConnectHandler
 
+# Replace host, username, password. Assumes a Priv15 username/password
 switch = {
     'device_type': 'cisco_ios',
     'host': '10.15.255.6',
@@ -15,4 +16,5 @@ config_commands = [ 'interface GigabitEthernet1/0/10',
 
 output = net_connect.send_config_set(config_commands)
 
+# Echos out the commands sent to the switch. Comment out to suppress
 print(output)
